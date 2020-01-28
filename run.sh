f90a3c08d3834b5ddf92a85c17fadfbf55e28724
Marlon Schroder <marlon.schroder@gmail.com>
2020-01-28 14:46:37 -0300

    upgrade

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@
 # wget https://dl.min.io/client/mc/release/linux-amd64/mc
 docker pull minio/minio
 
-docker run -p 80:9000 -v /minio:/data minio/minio server /data &
+docker run --rm -p 80:9000 -v /minio:/data minio/minio server /data &
